625840e4e54a78ea742b3c73c47e0c0d2bfbe177
MrSc33nsh0t <76527824+MrSc33nsh0t@users.noreply.github.com>
2024-02-24 21:07:01 +0100

    setup data

--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,27 @@
 const express = require("express");
 const app = express();
 
-const infos = {};
+function fetchJSONData(filename: string) {
+	fetch("./sample.json")
+		.then((res) => {
+			if (!res.ok) {
+				throw new Error
+					(`HTTP error! Status: ${res.status}`);
+			}
+			return res.json();
+		})
+		.then((data) => 
+			  console.log(data))
+		.catch((error) => 
+			   console.error("Unable to fetch data:", error));
+}
 
-app.get("/api", (req: any, res: any) => {
-	res.json(infos);
+app.get("/api/cars", (req: any, res: any) => {
+	res.json(fetchJSONData("./data/cars.json"));
+});
+
+app.get("/api/parts", (req: any, res: any) => {
+	res.json(fetchJSONData("./data/parts.json"));
 });
 
 app.listen(5000, () => {
