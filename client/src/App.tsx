26295965d8e637c6f735a78cbb8abb68883c30ec
MrSc33nsh0t <76527824+MrSc33nsh0t@users.noreply.github.com>
2024-02-24 19:46:13 +0100

    Revert "this commit is a mistake"
    This reverts commit 72a42f9c57e808a057cf0faaaedb7522439c9277.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { SetStateAction, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Heading } from "@chakra-ui/react";
 import { Header } from "./components/Header";
@@ -7,85 +7,37 @@ import "./App.css";
 import Home from "./pages/Home";
 
 function App() {
-	const [items, setItems] = useState(false);
-  
-	function getItems() {
-	  fetch('http://localhost:5000')
-		.then(response => {
-		  return response.text();
-		})
-		.then(data => {
-			setItems(data);
-		});
-	}
-  
-	function createItem() {
-	  let name = prompt('Enter merchant name');
-	  let email = prompt('Enter merchant email');
-	  fetch('http://localhost:5000/merchants', {
-		method: 'POST',
-		headers: {
-		  'Content-Type': 'application/json',
-		},
-		body: JSON.stringify({name, email}),
-	  })
-		.then(response => {
-		  return response.text();
-		})
-		.then(data => {
-		  alert(data);
-		  getItems();
-		});
-	}
-  
-	function deleteItem() {
-	  let id = prompt('Enter merchant id');
-	  fetch(`http://localhost:5000/merchants/${id}`, {
-		method: 'DELETE',
-	  })
-		.then(response => {
-		  return response.text();
-		})
-		.then(data => {
-		  alert(data);
-		  getItems();
-		});
-	}
-  
-	function updateItem() {
-	  let id = prompt('Enter merchant id');
-	  let name = prompt('Enter new merchant name');
-	  let email = prompt('Enter new merchant email');
-	  fetch(`http://localhost:5000/merchants/${id}`, {
-		method: 'PUT',
-		headers: {
-		  'Content-Type': 'application/json',
-		},
-		body: JSON.stringify({name, email}),
-	  })
-		.then(response => {
-		  return response.text();
-		})
-		.then(data => {
-		  alert(data);
-		  getItems();
-		});
-	}
-  
+	const [backendData, setBackendData] = useState([{}]);
+	const [isLoading, setLoading] = useState(true);
+
 	useEffect(() => {
-		getItems();
+		fetch("/api")
+			.then((response) => response.json())
+			.then((data) => {
+				setBackendData(data);
+				setLoading(false);
+				console.log(data.skills);
+				console.log(Object.entries(data.skills));
+			});
 	}, []);
 
+	if (isLoading) {
+		return (
+			<Heading className="App" size="7xl">
+				Loading...
+			</Heading>
+		);
+	}
+
 	return (
-	  <div>
-		{items ? items : 'There is no merchant data available'}
-		<br />
-		<button onClick={createItem}>Add item</button>
-		<br />
-		<button onClick={deleteItem}>Delete item</button>
-		<br />
-		<button onClick={updateItem}>Update item</button>
-	  </div>
+		<BrowserRouter>
+			<Header />
+			<Routes>
+				<Route path="/" element={<Home />} />
+			</Routes>
+			<Footer />
+		</BrowserRouter>
 	);
-  }
+}
+
 export default App;
--- server/server.ts
+++ server/server.ts
@@ -1,60 +1,83 @@
-import express from "express";
-
+const express = require("express");
 const app = express();
-const serverModel = require("./serverModel.js");
 
-app.use(express.json());
-app.use(function (req, res, next) {
-	res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
-	res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
-	next();
+const infos = {
+	formations: [
+		{
+			name: "Licence Informatique",
+			dates: "2021-2024",
+			description:
+				"L1 Mathématiques-Informatique puis L2-L3 Informatique",
+			logo: "https://www.univ-angers.fr/_resource/Documents/DCOM/logo/VERTICAL/PAPIER/JPEG/ua_v_couleur.jpg?download=true",
+		},
+		{
+			name: "Baccalauréat Général",
+			dates: "2021",
+			description:
+				"Spécialités Mathématiques / Numérique et Sciences Informatiques - Mention Bien",
+			logo: "https://pbs.twimg.com/profile_images/1060456926063009793/8QS24sxA_400x400.jpg",
+		},
+		{
+			name: "Certification TOEIC",
+			dates: "Janvier 2021",
+			description: "Score de 980/990 - Niveau C1",
+			logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/ETS_Logo.svg/2560px-ETS_Logo.svg.png",
+		},
+	],
+	experience: [
+		{
+			company: "Angers Loire Habitat",
+			jobTitle: "Chargé d’accueil et secrétariat",
+			dates: "2 au 25 août 2023",
+			logo: "https://baudet-sa.com/wp-content/uploads/2020/12/angers-loire-habitat.png",
+		},
+		{
+			company: "Verisure",
+			jobTitle: "Conseiller Technique et Relation Client",
+			dates: "13 juin au 19 août 2022",
+			description:
+				"- Prise en charge et résolution des besoins client\n- Orientation du client vers les services adéquats\n- Manipulations administratives relatives aux besoins client",
+			logo: "https://upload.wikimedia.org/wikipedia/commons/4/44/Verisure_Logo_-_2021-07-16.png",
+		},
+	],
+	skills: [
+		{
+			title: "POO",
+			list: ["C++", "C#", "Java"],
+		},
+		{
+			title: "Web",
+			list: ["HTML/CSS", "JavaScript", "PHP", "Astro", "React"],
+		},
+		{
+			title: "BDD",
+			list: ["SQL", "PostgreSQL"],
+		},
+		{
+			title: "Autres",
+			list: ["Python", "GitHub"],
+		},
+	],
+	interests: [
+		{
+			name: "Développement de jeux vidéos en amateur",
+		},
+		{
+			name: "Jeu de rôle sur table en association",
+			context: "(Héritiers de Dumnacus)",
+		},
+		{
+			name: "Worldbuilding",
+			context: "(projet communautaire The World Beyond The Ice Wall)",
+		},
+	],
+	languages: ["Français natif", "Anglais C2", "Espagnol A2"],
+};
+
+app.get("/api", (req: any, res: any) => {
+	res.json(infos);
 });
-
-app.get('/', (req, res) => {
-	serverModel.getItems()
-	.then((response: any) => {
-	  res.status(200).send(response);
-	})
-	.catch((error: any) => {
-	  res.status(500).send(error);
-	})
-})
-
-app.post('/merchants', (req, res) => {
-	serverModel.createItem(req.body)
-	.then((response: any) => {
-	  res.status(200).send(response);
-	})
-	.catch((error: any) => {
-	  res.status(500).send(error);
-	})
-  })
-  
-app.delete('/merchants/:id', (req, res) => {
-	serverModel.deleteItem(req.params.id)
-	.then((response: any) => {
-	  res.status(200).send(response);
-	})
-	.catch((error: any) => {
-	  res.status(500).send(error);
-	})
-  })
-
-app.put("/merchants/:id", (req, res) => {
-	const id = req.params.id;
-	const body = req.body;
-	serverModel
-	  .updateMerchant(id, body)
-	  .then((response: any) => {
-		res.status(200).send(response);
-	  })
-	  .catch((error: any) => {
-		res.status(500).send(error);
-	  });
-});
-  
 
 app.listen(5000, () => {
-  console.log("Server running on localhost:5000");
+	console.log("Server started on port 5000.");
 });
